f4daba09705d0d0ac550b434226adaedc16d0298
Gustavo Antonio Antunes Rodrigues <gustavo.rodrigues@restaurantemadero.com.br>
2025-04-14 09:49:57 -0300

    feat: ping controller

---
+++ src/CompostaAqui.Api/Controllers/PingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CompostaAqui.Api.Controllers
+{
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly JsonSerializerOptions _settings = new()
+        {
+            WriteIndented = true
+        };
+
+        public PingController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            var response = await Task.FromResult(new
+            {
+                AppName = AppDomain.CurrentDomain.FriendlyName,
+                Environment = _config["Environment"],
+                DateTimeOffset.Now,
+                Git = new
+                {
+                    ThisAssembly.Git.Branch,
+                    ThisAssembly.Git.Commit,
+                    ThisAssembly.Git.CommitDate,
+                    ThisAssembly.Git.IsDirty,
+                    ThisAssembly.Git.Sha
+                }
+            });
+
+            return new JsonResult(response, _settings);
+        }
+    }
+}
